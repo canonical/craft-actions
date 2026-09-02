3eac97cc2fc42862a1df2d0611b4c532cabc9156
Imani Pelton <imani.pelton@canonical.com>
2026-09-02 11:29:28 -0400

    feat: imagecraft/pack

---
+++ imagecraft/pack/src/index.ts
@@ -0,0 +1,18 @@
+import { CraftBuilder } from "@craft-actions/common/craft-builder.ts";
+import {
+  readBaseInputs,
+  runPackAction,
+} from "@craft-actions/common/pack-action.ts";
+
+export class ImagecraftBuilder extends CraftBuilder {
+  toolName = "imagecraft";
+  artifactOutput = { artifactType: ".img", outputName: "images" };
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  const builder = new ImagecraftBuilder({
+    ...readBaseInputs(),
+  });
+
+  void runPackAction(builder);
+}
